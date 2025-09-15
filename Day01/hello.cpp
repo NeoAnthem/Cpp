2997c7f355815bac80e7c8a64e9711b0d98d7a4e
Darshan Takarkhede <100999462+NeoAnthem@users.noreply.github.com>
2025-09-15 19:31:23 +0530

    Delete Day01 directory

--- Day01/hello.cpp
+++
@@ -1,9 +0,0 @@
-// WAP to print Hello World
-// printf -> cout (object) ostream -> iostream
-
-#include<iostream>
-using namespace std;
-int main(){
-    // std::cout << "Hello World"; // use if not using namespace std
-    cout << "Hello  World";
-}
