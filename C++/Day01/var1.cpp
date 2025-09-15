b1c469cd04fbcdd745b51cfb39d1e5f36d21e298
Darshan Takarkhede <100999462+NeoAnthem@users.noreply.github.com>
2025-09-15 19:28:36 +0530

    Add files via upload

---
+++ C++/Day01/var1.cpp
@@ -0,0 +1,20 @@
+/*
+1. Data Types -> int, char, float, double, bool
+2. Object -> string -> dynamic
+*/
+
+// inputs
+#include<iostream>
+using namespace std;
+int main() {
+    int age;
+    cout << "Enter your age : ";
+    cin >> age;
+
+    string name;
+    cout << "Enter your name : ";
+    cin >> name;
+
+    cout << "\nAge is " << age;
+    cout << "\nName is " << name;
+}
